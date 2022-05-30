10d7b2b5931877554a99a25d2ad4a8a7ac944437
X-Design <mxeland@gmail.com>
2022-05-30 21:08:11 +0200

    website1430052022-2

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,14 +1,21 @@
 //! === ADD HEADER STYLES ON SCROLL ===
+
 const headerScroll = () => {
 	const headerElement = document.querySelector('.header');
 	this.scrollY >= 30 ? headerElement.classList.add('active') : headerElement.classList.remove('active');
 }
-
-	;
 window.addEventListener('scroll', headerScroll);
 
 
 //! === OPEN AND CLOSE THE MENU ON HAMBURGER ICON CLICK ===
+
+const menuToggler = document.querySelector('#menu-toggler');
+const navbarMenu = document.querySelector('#navbar__menu');
+const toggleMenu = () => {
+	navbarMenu.classList.toggle('active');
+};
+menuToggler.addEventListener('click', toggleMenu);
+
 
 // --- CLOSE MENU WHEN NAV-LINKS ARE CLICKED ---
 
