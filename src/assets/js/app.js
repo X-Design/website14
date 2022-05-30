8daad5abc22f34d4519834200509e393a99130b1
X-Design <mxeland@gmail.com>
2022-05-30 16:27:12 +0200

    website14-30052022

---
+++ src/assets/js/app.js
@@ -0,0 +1,17 @@
+//! === ADD HEADER STYLES ON SCROLL ===
+const headerScroll = () => {
+	const headerElement = document.querySelector('.header');
+	this.scrollY >= 30 ? headerElement.classList.add('active') : headerElement.classList.remove('active');
+}
+
+	;
+window.addEventListener('scroll', headerScroll);
+
+
+//! === OPEN AND CLOSE THE MENU ON HAMBURGER ICON CLICK ===
+
+// --- CLOSE MENU WHEN NAV-LINKS ARE CLICKED ---
+
+//! === SWIPER ===
+
+//! === SCROLL REVEAL ===
