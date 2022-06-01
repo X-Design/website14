7c04451a522ac5e70db62c589fa0f920a7dd5cc8
X-Design <mxeland@gmail.com>
2022-06-01 20:07:32 +0200

    website14 -1 01062022

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -3,14 +3,14 @@
 const headerScroll = () => {
 	const headerElement = document.querySelector('.header');
 	this.scrollY >= 30 ? headerElement.classList.add('active') : headerElement.classList.remove('active');
-}
+};
 window.addEventListener('scroll', headerScroll);
 
 
 //! === OPEN AND CLOSE THE MENU ON HAMBURGER ICON CLICK ===
 
 const menuToggler = document.querySelector('#menu-toggler');
-const navbarMenu = document.querySelector('#navbar__menu');
+const navbarMenu = document.querySelector('.navbar__menu');
 const toggleMenu = () => {
 	navbarMenu.classList.toggle('active');
 };
@@ -18,6 +18,10 @@ menuToggler.addEventListener('click', toggleMenu);
 
 
 // --- CLOSE MENU WHEN NAV-LINKS ARE CLICKED ---
+const linksToggleMenu = (e) => {
+	if (e.target.classList.contains('navbar__list-link')) navbarMenu.classList.remove('active');
+};
+window.addEventListener('click', linksToggleMenu);
 
 //! === SWIPER ===
 
